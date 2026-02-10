0241eb1cafeec45c6b64bb1f913162e7f5fd6abe
Duoiz <Duoisarfolsio@gmail.com>
2026-02-10 21:19:50 +0800

    smol clean up on alert

--- components/auth/AuthModal.tsx
+++ components/auth/AuthModal.tsx
@@ -73,7 +73,7 @@ export default function AuthModal({
 
   const handleFinalize = async () => {
     if (!formData.email || !formData.password) {
-      Alert.prompt("Missing Info", "Please complete all required fields.");
+      Alert.alert("Missing Info", "Please complete all required fields.");
       return;
     }
 
